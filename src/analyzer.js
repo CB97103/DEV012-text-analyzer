feca67d9cd9e166f1d29ce376b46b1c79447f082
CB97103 <gxguaqueta@soy.sena.edu.co>
2023-09-27 13:10:47 -0500

    segundoSpring

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,14 +1,28 @@
-const analyzer = {  
+const analyzer = {
+  metodoUno: (texto) => {
+    return "Me gusta la " + texto;
+  },
+
   getWordCount: (text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    const totalCarac = numeroDeCaracteres.value.length;
+    document.getElementById("resultado").innerHTML = totalCarac;
+    
+
   },
+
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+   
+   
+    /*getCharacterCount.addEventListener(textarea, function (){
+      const total = getCharacterCount.value.lenght;
+      document.getElementBy(".opcion").innerHTML = total;*/
   },
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
   },
-  getAverageWordLength: (text) => {    
+  getAverageWordLength: (text) => {
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
   getNumberCount: (text) => {
--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+/*Function contarLetras (){
+        document.querySelectorAll("textarea").value;
+   numeroDeCaracteres = texto.lenght;
+console.log("numeroDeCaracteres");
+}*/
+
+const boton = document.getElementById("reset-button");
+const textarea = document.querySelector ('textarea[name="user-input"]');
+
+const primerElemento = document.querySelector('li.conteo:nth-child(1)') /*se ubica en la posicion*/
+const segundoElemento = document.querySelector('li.conteo:nth-child(3)')
+/*const getCharacterCount = document.querySelector ('li.conteo:nth-child(2)')*/
+
+boton.addEventListener('click', function (){
+textarea.value = '';
+
+})
+
+textarea.addEventListener('input', function( ){
+const numeroDeCaracteres = analyzer.getCharacterCount('textarea[name="user-input"]')
+
+
+
+})
+        /*
+        const totalCaracteres = analyzer.metodoUno("uva");
+primerElemento.textContent = totalCaracteres;
+segundoElemento.textContent = 'Me gusta la pera';
+})
+*/
+
+
+
